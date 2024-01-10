08a5985137efdd467b52cece0dd6c473c805b6c6
Vassily Petrousevitch <90280831+Vassily-Petrousevitch@users.noreply.github.com>
2024-01-10 09:30:45 -0800

    Imported cypress-real-events for add-page

--- cypress/e2e/add-page/add-page.cy.js
+++ cypress/e2e/add-page/add-page.cy.js
@@ -11,77 +11,59 @@ describe("Does add page stuff", {testIsolation: false}, () => {
         const titleMessage = "Title goes here";
         const summaryMessage = "Summary text goes here";
 
-        cy.get('.form-item--title-0-value > input').type(titleMessage).should('have.value', titleMessage);
-        cy.get('#edit-body-0-summary').type(summaryMessage).should('have.value', summaryMessage);
+        cy.get('[data-drupal-selector="edit-title-wrapper"] input').type(titleMessage).should('have.value', titleMessage);
+        cy.get('[data-drupal-selector="edit-body-0-summary"]').type(summaryMessage).should('have.value', summaryMessage);
     })
 
     it("Tries to type in the body text box", () => {
         const bodyMessage = "This text goes in the body";
 
-        // Gotta grab the ckeditor using this method (get the actual div that contains <p>)
-        cy.get('.ck-editor__main > .ck-editor__editable').then(el => {
+        cy.get('.ck[role="textbox"]').realClick().realType(bodyMessage);
+        cy.get('.ck[role="textbox"]').then(el => {
             const editor = el[0].ckeditorInstance;
-            editor.setData(bodyMessage);
-            // TODO: how to assert that the body element was typed inside?
-        })        
+
+            // Test whether typing was successful
+            const data = editor.getData();
+            expect(data).to.contain(bodyMessage);
+        });
     })
 
-    // TODO: look at  "How to find all specific elements in the editor?"
-    it("Inserts two columns widget", () => {
-        // Spawns in the two columns widget
-        cy.get('.cke5-ubccolumnstwo-insert-button .ck-icon').click()
+    // it("Inserts two columns widget", () => {
+    //     // Spawns in the two columns widget
+    //     cy.get('.cke5-ubccolumnstwo-insert-button .ck-icon').click()
 
-        // Interacts with the two columns widget (TODO: implement this properly)
+    //     // Interacts with the two columns widget (TODO: implement this properly)
+    // })
 
-        
-        // cy.get('.ck-editor__main > .ck-editor__editable').then(el => {
-        //     const editor = el[0].ckeditorInstance;
-        //     editor.model.change( writer => {
-                // const insertPosition = editor.model.document.selection.getSelectedElement();
-                // const text = writer.createText( 'foo' );
-                // writer.insert( text, insertPosition, 0 );
+    // it("Inserts three columns widget", () => {
+    //     cy.get('.cke5-ubccolumnsthree-insert-button .ck-icon').click()
+    // })
 
-                // const more_text = writer.createText('bar');
-                // writer.insert( more_text, insertPosition.nodeAfter, 0);
+    // it("Inserts four columns widget", () => {
+    //     cy.get('.cke5-ubccolumnsfour-insert-button .ck-icon').click()
+    // })
 
-                // const range = editor.model.createRangeIn( editor.model.document.getRoot() );
+    // it("Inserts an accordian toggle", () => {
+    //     cy.get('.cke5-ubcaccordiontoggle-insert-button').click()
+    // })
 
-                // for ( const value of range.getWalker() ) {
-                //     cy.log(value);
-                // }
-        //     } );
-        // })
-    })
+    // it("Inserts an accordian", () => {
+    //     cy.get('.cke5-ubcaccordion-insert-button').click()
+    // })
 
-    it("Inserts three columns widget", () => {
-        cy.get('.cke5-ubccolumnsthree-insert-button .ck-icon').click()
-    })
+    // it("Inserts a horizontal card", () => {
+    //     cy.get('.cke5-ubccardhorizontal-insert-button').click()
+    // })
 
-    it("Inserts four columns widget", () => {
-        cy.get('.cke5-ubccolumnsfour-insert-button .ck-icon').click()
-    })
+    // it("Inserts a vertical card", () => {
+    //     cy.get('.cke5-ubccardverticalone-insert-button').click()
+    // })
 
-    it("Inserts an accordian toggle", () => {
-        cy.get('.cke5-ubcaccordiontoggle-insert-button').click()
-    })
+    // it("Inserts two vertical cards", () => {
+    //     cy.get('.cke5-ubccardverticaltwo-insert-button').click()
+    // })
 
-    it("Inserts an accordian", () => {
-        cy.get('.cke5-ubcaccordion-insert-button').click()
-    })
-
-    it("Inserts a horizontal card", () => {
-        cy.get('.cke5-ubccardhorizontal-insert-button').click()
-    })
-
-    it("Inserts a vertical card", () => {
-        cy.get('.cke5-ubccardverticalone-insert-button').click()
-    })
-
-    it("Inserts two vertical cards", () => {
-        cy.get('.cke5-ubccardverticaltwo-insert-button').click()
-    })
-
-    it("Inserts three vertical cards", () => {
-        cy.get('.cke5-ubccardverticalthree-insert-button').click()
-    })
+    // it("Inserts three vertical cards", () => {
+    //     cy.get('.cke5-ubccardverticalthree-insert-button').click()
+    // })
 })
--- cypress/e2e/cron/cron.cy.js
+++ cypress/e2e/cron/cron.cy.js
@@ -1,23 +1,17 @@
 import { doLogin } from "../helpers/helpers.cy.js"
 
 describe('Generic Test Suite - Cron', () => {
-
-    Cypress.on('uncaught:exception', (err, runnable) => {
-        return false;
-    })
-
     beforeEach((() => {
         cy.doLogin();
         cy.visit('admin/config/system/cron');
     }))
 
-    it('can run cron with button', () => {
+    it('Runs cron -> checks whether run was completed (admin)', () => {
+        // TODO: make ALL cy.get selectors better (minimum use ids)
+
         cy.get('.page-title').contains('Cron');
-        // cy.get('.messages').should('not.exist');     // TODO: sometimes errors show up but we still wanna run the test
         cy.get('#edit-run').click();
-        // cy.wait(5000);
-        // // Message may not be visible on local environment
-        //cy.get('.messages').contains('Cron ran successfully.');
+        cy.get('.messages').contains('Cron ran successfully.');
         cy.visit('admin/reports/dblog');
         cy.contains('td.views-field.views-field-message a', 'Cron run completed.', { first: true });
     })
